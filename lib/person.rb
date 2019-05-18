4cea9d9c41190130be743986a2c40f33d284f6ac
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-18 23:44:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,17 @@
 class Person
-
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
-
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
-
   def name
-    "#{@first_name} #{@last_name}".strip
+    @name
   end
 
+  def name=(name)
+    @name = name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
 end
